e386b1d58379473502a46c56125c6ae0ccfd42fc
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2022-09-11 22:17:31 -0500

    testing

--- src/Test.java
+++ src/Test.java
@@ -1,11 +1,20 @@
 import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
-        int a = 3;
-        byte b = (byte)7;
-        long c = 234l;
-        double d = 324.234;
-        System.out.println(-24 % -5);
+        boolean first, second, third, fourth;
+        first = second = third = fourth = true;
+        for(int x = -100; x<100; x++){
+            for(int y = -100; y<100; y++) {
+                first = first && (((x > 0 || x < 10) == ((x > 0) || (x < 10)))) ? true : false;
+                second = second && ((x > 0 || x < 10 && y < 0) == (x > 0 || (x < 10 && y < 0))) ? true : false;
+                third = third && ((x > 0 && x < 10) == ((x > 0) && (x < 10))) ? true : false;
+                fourth = fourth && ((x > 0 || x < 10 && y < 0) == ((x > 0 || x < 10) && y < 0)) ? true : false;
+            }
+        }
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(fourth);
     }
 
     public void testingmethod(){
