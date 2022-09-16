a2bcf7f619a599cbc3bcfccd06ff9b05054b7bf9
Jason Howse <78813074+JasonWHowse@users.noreply.github.com>
2022-09-16 11:02:25 -0500

    Module 4 Update

--- src/Exercise04_15.java
+++ src/Exercise04_15.java
@@ -1,4 +1,3 @@
-import java.util.Dictionary;
 import java.util.Hashtable;
 import java.util.Scanner;
 public class Exercise04_15 {
--- src/Test.java
+++ src/Test.java
@@ -1,22 +1,39 @@
 import java.util.Scanner;
 public class Test {
     public static void main(String[] args) {
-        boolean first, second, third, fourth;
-        first = second = third = fourth = true;
-        for(int x = -100; x<100; x++){
-            for(int y = -100; y<100; y++) {
-                first = first && (((x > 0 || x < 10) == ((x > 0) || (x < 10)))) ? true : false;
-                second = second && ((x > 0 || x < 10 && y < 0) == (x > 0 || (x < 10 && y < 0))) ? true : false;
-                third = third && ((x > 0 && x < 10) == ((x > 0) && (x < 10))) ? true : false;
-                fourth = fourth && ((x > 0 || x < 10 && y < 0) == ((x > 0 || x < 10) && y < 0)) ? true : false;
-            }
+        try {
+            System.out.printf("%10.2e");
+        } catch (Exception err){
+            System.out.println("%10.2e");
+            System.out.println(err);
         }
-        System.out.println(first);
-        System.out.println(second);
-        System.out.println(third);
-        System.out.println(fourth);
-    }
+        try {
+            System.out.printf("%8.2d");
+        } catch (Exception err){
+            System.out.println("%8.2d");
+            System.out.println(err);
+        }
+        try {
+            System.out.printf("%10b");
+        } catch (Exception err){
+            System.out.println("%10b");
+            System.out.println(err);
+        }
+        try {
+            System.out.printf("%4c");
+        } catch (Exception err){
+            System.out.println("%4c");
+            System.out.println(err);
+        }
+        try {
+            System.out.printf("%6d");
+        } catch (Exception err){
+            System.out.println("%6d");
+            System.out.println(err);
+        }
 
+
+    }
     public void testingmethod(){
 
     }
